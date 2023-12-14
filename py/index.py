f8901aac275dd319a3c7d841b84ec66afbc2ec0a
Thimea <84723445+Thimea5@users.noreply.github.com>
2023-12-14 22:54:20 +0100

    Supression  du test python

--- py/index.py
+++
@@ -1,8 +0,0 @@
-#Ici on met aucun code sauf pour inclure les fichiers (genre menu.py, jeu.py ....)
-#Pour lancer l'appli sur VSCODE : "run python file in dedicated terminal"
-
-#Les imports
-from HomePage import *
-
-newHomePage = HomePage()
-newHomePage.show()
